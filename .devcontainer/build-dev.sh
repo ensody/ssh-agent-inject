74c825a8e0ec329dda0645e48aae71043c3872a7
Waldemar Kornewald <wkornewald@gmail.com>
2019-09-12 09:45:36 +0000

    Upgrade to Go 1.13

--- .devcontainer/build-dev.sh
+++ .devcontainer/build-dev.sh
@@ -28,37 +28,3 @@ DIFF_SO_FANCY_VERSION="1.2.6"
 download https://raw.githubusercontent.com/so-fancy/diff-so-fancy/v${DIFF_SO_FANCY_VERSION}/third_party/build_fatpack/diff-so-fancy \
   ed9de2669c789d1aba8456d0a7cf95adb326e220c99af4336405f21add8f0852 /usr/bin/diff-so-fancy
 chmod a+x /usr/bin/diff-so-fancy
-
-# The following is taken from
-# https://github.com/microsoft/vscode-remote-try-go/blob/master/.devcontainer/Dockerfile
-
-# Install gocode-gomod
-go get -x -d github.com/stamblerre/gocode 2>&1
-go build -o gocode-gomod github.com/stamblerre/gocode
-mv gocode-gomod $GOPATH/bin/
-
-# Install Go tools
-go get -u -v \
-  github.com/mdempsky/gocode \
-  github.com/uudashr/gopkgs/cmd/gopkgs \
-  github.com/ramya-rao-a/go-outline \
-  github.com/acroca/go-symbols \
-  github.com/godoctor/godoctor \
-  golang.org/x/tools/cmd/guru \
-  golang.org/x/tools/cmd/gorename \
-  github.com/rogpeppe/godef \
-  github.com/zmb3/gogetdoc \
-  github.com/haya14busa/goplay/cmd/goplay \
-  github.com/sqs/goreturns \
-  github.com/josharian/impl \
-  github.com/davidrjenni/reftools/cmd/fillstruct \
-  github.com/fatih/gomodifytags \
-  github.com/cweill/gotests/... \
-  golang.org/x/tools/cmd/goimports \
-  golang.org/x/lint/golint \
-  golang.org/x/tools/cmd/gopls \
-  github.com/alecthomas/gometalinter \
-  honnef.co/go/tools/... \
-  github.com/golangci/golangci-lint/cmd/golangci-lint \
-  github.com/mgechev/revive \
-  github.com/derekparker/delve/cmd/dlv
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ensody/ssh-agent-inject
 
-go 1.12
+go 1.13
 
 require (
 	github.com/Microsoft/go-winio v0.4.14
